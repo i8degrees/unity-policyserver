7a320fcec237d52d6f7dfd049a5bf29b39609fea
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-09-07 23:14:42 -0500

    Move server logic into PolicyServer namespace
    * Introduce optional SOCKET_SERVER_TIMEOUT env var.
    
    * Update README.md (add contributing, license info).

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Usage
 
-Create crossdomain.xml in same directory as server.js, if you do not have an existing one:
+Create ```crossdomain.xml``` in same directory as server.js, if you do not have an existing one:
 
 ```
 touch crossdomain.xml
@@ -16,19 +16,60 @@ sudo node server.js
 Verifying working state:
 
 ```
-node client.js
+node tests/client.js
 ```
 
 ## Notes
 
 * Uses crossdomain.xml example from http://docs.unity3d.com/Manual/SecuritySandbox.html
 
-* It is strongly recommended that you change the default listening port number from 843 to something above 1024, as this allows you to run with reduced privileges (i.e.: not root).
+* It is strongly recommended that you change the default listening port number from 843 to something above 1024, as this allows you to run with reduced privileges (i.e.: not root). See also: http://docs.unity3d.com/ScriptReference/Security.PrefetchSocketPolicy.html
 
 * Connections via telnet does **not** work.
 
 ## TODO
 
 * Package
-* Integration with express? 
-* Unit tests
+* Example of using app with Express? 
+
+## Contributing
+
+Fork [project](https://github.com/i8degrees/unity-policyserver/) from the web.
+
+Create a new branch (i.e.: bugfix/font-rendering):
+
+```
+git checkout -b bugfix/font-rendering
+```
+
+Make your changes.
+
+Commit your changes to your new branch:
+
+```
+git commit -am 'Fix font rendering issue'
+```
+
+Push changes to your remote branch:
+
+```
+git push origin bugfix/font-rendering
+```
+
+Create a [Pull Request](https://github.com/i8degrees/unity-policyserver/pulls/).
+
+## License (Simplified BSD)
+
+Copyright (c) 2014, Jeffrey Carpenter <i8degrees@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
--- server.js
+++ server.js
@@ -3,10 +3,59 @@
 //
 // Reference implementation: https://gist.githubusercontent.com/mattcg/489841/raw/15310d8218f5457af029fddcd90b2f52a1beb97c/node_fsps.js
 //
-// See also: http://nodejs.org/api/net.html
-//
 
+// Dependencies
 var fs = require('fs');
+var net = require('net');
+
+var PolicyServer = module.exports = {
+
+  // File contents
+  fp_buffer: null,
+
+  listen: function listen( params ) {
+
+    fp_buffer = fs.readFileSync( params.file, 'utf8' );
+
+    var server = net.createServer( function ( stream ) {
+      stream.setEncoding( 'utf8' );
+      stream.setTimeout( params.timeout ); // 3s
+
+      console.log( 'Established connection from ' + stream.remoteAddress + '.' );
+
+      stream.on('data', function ( command ) {
+      if( command == '<policy-file-request/>\0' ) {
+        console.log( 'Accepted request from ' + stream.remoteAddress + '.')
+        stream.end( fp_buffer + '\0' );
+      }
+      else {
+        console.log( command.toString() );
+        console.log('Bad request from ' + stream.remoteAddress + '.');
+        stream.end();
+      }
+    });
+
+      stream.on('end', function() {
+        stream.end();
+      });
+
+      stream.on('timeout', function() {
+        console.log('Request from ' + stream.remoteAddress + ' timed out.');
+        stream.end();
+      });
+    }); // end var server
+
+    // FIXME
+    // process.setgid('nobody');
+    // process.setuid('nobody');
+
+    server.listen( params.port, params.host );
+
+    console.log( 'Unity Web Player socket policy server listening at ' + params.host + ':' + params.port );
+
+  }, // end function listen
+
+}; // end function PolicyServer
 
 // Environment defaults
 var file = process.env.SOCKET_SERVER_FILE || './crossdomain.xml';
@@ -14,44 +63,13 @@ var file = process.env.SOCKET_SERVER_FILE || './crossdomain.xml';
 // 0.0.0.0 is what works for Stew
 var host = process.env.SOCKET_SERVER_HOST || 'localhost';
 var port = process.env.SOCKET_SERVER_PORT || 843;
+var timeout = process.env.SOCKET_SERVER_TIMEOUT || 3000;
 
-// File contents
-var fp_buffer;
+var params = {
+  host: host,
+  port: port,
+  file: file,
+  timeout: timeout,
+};
 
-var policy_server = require('net').createServer(function (stream) {
-  stream.setEncoding( 'utf8' );
-  stream.setTimeout( 3000 ); // 3s
-
-  console.log('Established connection from ' + stream.remoteAddress + '.');
-
-  stream.on('data', function ( command ) {
-    if( command == '<policy-file-request/>\0' ) {
-      console.log('Good request; sending file to ' + stream.remoteAddress + '.')
-      stream.end( fp_buffer + '\0' );
-    }
-    else {
-      console.log( command.toString() );
-      console.log('Bad request from ' + stream.remoteAddress + '.');
-      stream.end();
-    }
-  });
-
-  stream.on('end', function() {
-    stream.end();
-  });
-
-  stream.on('timeout', function() {
-    console.log('Request from ' + stream.remoteAddress + ' timed out.');
-    stream.end();
-  });
-});
-
-fp_buffer = fs.readFileSync(file, 'utf8');
-
-policy_server.listen( port, host );
-
-// FIXME
-// process.setgid('nobody');
-// process.setuid('nobody');
-
-console.log( 'Unity Web Player socket policy server running at ' + host + ':' + port + ' and serving ' + file );
+var policy_server = PolicyServer.listen( params );
